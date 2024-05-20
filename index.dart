d1d42aa6eccb4abff014ac8dca8ca5e941f13a10
Hattem10 <120330405+Hattem10@users.noreply.github.com>
2024-05-21 01:46:07 +0300

    Add files via upload

---
+++ index.dart
@@ -0,0 +1,23 @@
+// Export pages
+export '/pages/choice_page/choice_page_widget.dart' show ChoicePageWidget;
+export '/pages/success_page/success_page_widget.dart' show SuccessPageWidget;
+export '/pages/sign_in/sign_in_widget.dart' show SignInWidget;
+export '/pages/sign_up/sign_up_widget.dart' show SignUpWidget;
+export '/pages/account/account_widget.dart' show AccountWidget;
+export '/pages/explore_page/explore_page_widget.dart' show ExplorePageWidget;
+export '/pages/edit/edit_widget.dart' show EditWidget;
+export '/pages/search_page/search_page_widget.dart' show SearchPageWidget;
+export '/pages/choose_location/choose_location_widget.dart'
+    show ChooseLocationWidget;
+export '/pages/location_update/location_update_widget.dart'
+    show LocationUpdateWidget;
+export '/pages/language_changer/language_changer_widget.dart'
+    show LanguageChangerWidget;
+export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/pages/season/season_widget.dart' show SeasonWidget;
+export '/pages/soil_details/soil_details_widget.dart' show SoilDetailsWidget;
+export '/pages/season_update/season_update_widget.dart' show SeasonUpdateWidget;
+export '/pages/favorites/favorites_widget.dart' show FavoritesWidget;
+export '/pages/f_a_qs/f_a_qs_widget.dart' show FAQsWidget;
+export '/pages/expertise_l_v_l/expertise_l_v_l_widget.dart'
+    show ExpertiseLVLWidget;
